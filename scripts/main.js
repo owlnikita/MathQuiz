a08467a015c472d56cd9953fe96d03d076d58215
owlnikita <hrymkanom@gmail.com>
2023-04-08 20:58:41 +0600

    file update

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,10 @@ let containerH3 = document.querySelector('.container_h3')
 let h3Text = document.querySelector('.h3-text')
 let startBtn = document.querySelector('.start_btn')
 
+let cookie = false;
+let cookies = document.cookie.split('; ');
+
+
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
     return array;
@@ -58,6 +62,27 @@ class Question {
     }
 };
 
+for (let i = 0; i < cookies.length; i++){
+    if(cookies[i].split('=')[0] == 'numbers_high_score'){
+        cookie = cookies[i].split('=')[1]
+        break;
+    }
+}
+
+
+
+if (cookie){
+    console.log(cookie)
+    console.log(h3Text)
+    containerH3.style.display = 'flex'
+    let data = cookie.split('/');
+    let statisticCookie = Math.round(data[1]*100/data[0])
+    if (data[1] == 0){statisticCookie = 0}
+    h3Text.innerHTML = `В прошлый раз вы дали ${data[1]}
+                             правильных ответов из ${data[0]}.
+                             Точность - ${statisticCookie}%`
+}
+
 
 
 let currentQuestion = new Question()
@@ -65,20 +90,35 @@ currentQuestion.display();
 let correctAnswers = 0;
 let totalAnswers = 0;
 
+window.onload = function() {
+    anime({
+        targets: startBtn,
+        maxWidth: `478px`,
+        padding: `23px 81px`,
+        fontSize: `64px`,
+        duration: 500,
+        delay: 100,
+        easing: 'linear',
+    })
+};
+  
+
 startBtn.addEventListener('click', ()=>{
     main.style.display = 'flex'
     start.style.display = 'none'
     h3Text.innerHTML = `` 
     
     setTimeout(() =>{
+        let newCookie = `numbers_high_score=${totalAnswers}/${correctAnswers}; max-age=240`
         let statistic = Math.round(correctAnswers*100/totalAnswers)
         if (totalAnswers === 0){statistic=0}
-        h3Text.innerHTML += `Вы дали ${correctAnswers} правильных ответов из ${totalAnswers}.</br>Точность - ${statistic}%` 
+        h3Text.innerHTML = `Вы дали ${correctAnswers} правильных ответов из ${totalAnswers}.</br>Точность - ${statistic}%` 
         start.style.display = 'block'
         main.style.display = 'none'
         containerH3.style.display = 'block'
         correctAnswers = 0
         totalAnswers = 0
+        document.cookie = newCookie
 }, 15000)
 })
 
